d687fe9aa50626235dc270bbf03d96a9a559e71f
Taj English <tajenglish@gmail.com>
2016-10-13 11:51:49 -0400

    Added Card Creating Logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,4 +5,14 @@ var cardFour = "";
 if (cardTwo === cardFour){
   alert("You found a match!");
 
+}else{
+
+  alert("Sorry, try again");
+}
+var cardLoc = document.getElementById('game-board');
+
+for (var i = 0; i < 4; i++){
+  var cards = document.createElement('div');
+  cards.className = "card";
+  cardLoc.appendChild(cards);
 }
